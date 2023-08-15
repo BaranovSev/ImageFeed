ded9423edc70b7db53be44f125e626befad3780f
BaranovSev <aldebaran94sev@gmail.com>
2023-08-15 22:57:22 +0300

    Single image controller & navigation added.

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Stepan Baranov on 15.08.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet weak private var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
